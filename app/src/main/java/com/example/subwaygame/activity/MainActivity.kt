00ac37a4264ae4e49cf5104c78ad3f08fbed94f7
kirito <45502455+kirito0206@users.noreply.github.com>
2020-03-28 15:36:05 +0800

    初始化

--- app/src/main/java/com/example/subwaygame/activity/MainActivity.kt
+++ app/src/main/java/com/example/subwaygame/activity/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.example.subwaygame
+package com.example.subwaygame.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.subwaygame.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
---
+++ app/src/main/java/com/example/subwaygame/data/Plager.java
@@ -0,0 +1,10 @@
+package com.example.subwaygame.data;
+
+import java.util.ArrayList;
+
+public class Plager {
+
+    private String name;
+    private String headPicture;
+    private ArrayList<String>
+}
