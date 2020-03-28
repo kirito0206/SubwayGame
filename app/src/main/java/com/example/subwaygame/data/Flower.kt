a60365cbe049d288c12571a13e52c4caf1c84bdf
kirito <45502455+kirito0206@users.noreply.github.com>
2020-03-28 17:50:41 +0800

    添加了图片

---
+++ app/src/main/java/com/example/subwaygame/data/Flower.kt
@@ -0,0 +1,7 @@
+package com.example.subwaygame.data
+
+data class Flower (
+    var name : String,
+    var stage : String,
+    var waterNumber : Int
+)
--- app/src/main/java/com/example/subwaygame/data/Plager.java
+++ app/src/main/java/com/example/subwaygame/data/Plager.java
@@ -1,10 +1,20 @@
 package com.example.subwaygame.data;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+
 import java.util.ArrayList;
 
+@Entity
 public class Plager {
-
+    @Id
     private String name;
     private String headPicture;
-    private ArrayList<String>
+    private ArrayList<String> achievementList;
+    private ArrayList<Flower> flowerList;
+    private Flower flower;
+
+    @Generated
+    public Plager(){}
 }
