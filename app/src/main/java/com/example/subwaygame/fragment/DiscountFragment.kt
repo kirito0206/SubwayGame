16d75fdb8c78d916127d916f15d7cdfa450da0c0
kirito <45502455+kirito0206@users.noreply.github.com>
2020-04-03 15:48:57 +0800

    图标图片更新

--- app/src/main/java/com/example/subwaygame/fragment/DiscountFragment.kt
+++ app/src/main/java/com/example/subwaygame/fragment/DiscountFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridView
 import androidx.fragment.app.Fragment
 import com.example.subwaygame.MyApplication
 import com.example.subwaygame.R
 import com.example.subwaygame.adapter.GiftGridAdapter
 import com.example.subwaygame.data.Gift
-import kotlinx.android.synthetic.main.fragment_item.*
 import kotlinx.android.synthetic.main.fragment_item.view.*
 
 
@@ -18,11 +16,11 @@ class DiscountFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_item, container, false)
         var giftList = ArrayList<Gift>()
-        giftList.add(Gift(R.drawable.water,"地铁出行八折券","需要鲜花*1"))
-        giftList.add(Gift(R.drawable.water,"快车出行八折券","需要鲜花*1"))
-        giftList.add(Gift(R.drawable.water,"共享单车八折券","需要鲜花*1"))
-        giftList.add(Gift(R.drawable.water,"金拱门八折券","需要鲜花*1"))
-        giftList.add(Gift(R.drawable.water,"星巴克八折券","需要鲜花*1"))
+        giftList.add(Gift(R.drawable.ticket,"地铁出行八折券","需要鲜花*1"))
+        giftList.add(Gift(R.drawable.ticket,"快车出行八折券","需要鲜花*1"))
+        giftList.add(Gift(R.drawable.ticket,"共享单车八折券","需要鲜花*1"))
+        giftList.add(Gift(R.drawable.ticket,"金拱门八折券","需要鲜花*1"))
+        giftList.add(Gift(R.drawable.ticket,"星巴克八折券","需要鲜花*1"))
         view.grid_view.adapter = GiftGridAdapter(MyApplication.getContext(),giftList)
         return view
     }
--- app/src/main/java/com/example/subwaygame/fragment/FlowerFragment.kt
+++ app/src/main/java/com/example/subwaygame/fragment/FlowerFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridView
 import androidx.fragment.app.Fragment
 import com.example.subwaygame.MyApplication
 import com.example.subwaygame.R
 import com.example.subwaygame.adapter.GiftGridAdapter
 import com.example.subwaygame.data.Gift
-import kotlinx.android.synthetic.main.fragment_item.*
 import kotlinx.android.synthetic.main.fragment_item.view.*
 
 class FlowerFragment : Fragment() {
@@ -18,8 +16,8 @@ class FlowerFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_item, container, false)
         var giftList = ArrayList<Gift>()
-        giftList.add(Gift(R.drawable.one_6,"地铁纪念钥匙扣","需要鲜花*3"))
-        giftList.add(Gift(R.drawable.one_6,"地铁纪念U盘","需要鲜花*5"))
+        giftList.add(Gift(R.drawable.keycircle,"地铁纪念钥匙扣","需要鲜花*3"))
+        giftList.add(Gift(R.drawable.u,"地铁纪念U盘","需要鲜花*5"))
         giftList.add(Gift(R.drawable.one_6,"百合花","需要鲜花*3"))
         giftList.add(Gift(R.drawable.one_6,"海棠花","需要鲜花*3"))
         giftList.add(Gift(R.drawable.one_6,"水仙花","需要鲜花*3"))
